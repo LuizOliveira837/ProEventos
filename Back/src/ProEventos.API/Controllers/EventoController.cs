df71fd79e758fedcb7f40aca2afdeb1150950089
LuizOliveira837 <luizhorocha@gmail.com>
2022-08-23 00:18:38 -0300

    Adicionando conexão com o SQLITE utilizando o EF CORE

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,34 +13,11 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-
-
-        public IEnumerable<Evento> _evento = new Evento[]{
-                
-            new Evento(){
-                EventoId = 1,
-                DataEvento = new DateTime().ToString(),
-                Local = "São Paulo",
-                QtdPessoas = 30,
-                Tema = "Halloween",
-                Lote = "L12230" ,
-                ImagemURL = "Luuioxjij"          
-                
-            },
-             new Evento(){
-                EventoId = 2,
-                DataEvento = new DateTime().ToString(),
-                Local = "São Paulo",
-                QtdPessoas = 30,
-                Tema = "Halloween",
-                Lote = "L12230" ,
-                ImagemURL = "Luuioxjij"          
-                
-            }
-        };
+        public readonly DataContext _context;
        
-        public EventoController(ILogger<EventoController> logger)
+        public EventoController(DataContext context)
         {
+            _context = context;
           
         }
 
@@ -47,15 +25,15 @@ namespace ProEventos.API.Controllers
         public IEnumerable<Evento> Get()
         {
 
-            return _evento;
+            return _context.Eventos;
             
         }
 
         [HttpGet("{id}")]
-         public IEnumerable<Evento> GetById(int id)
+         public Evento GetById(int id)
         {
 
-            return _evento.Where(evento=> evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento=> evento.EventoId == id);
             
         }
 
--- Back/src/ProEventos.API/Models/Evento.cs
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -7,10 +7,14 @@ namespace ProEventos.API.Models
         public string Local { get; set; }
 
         public string DataEvento { get; set; }
+        
 
         public string Tema { get; set; }
+
         public int QtdPessoas { get; set; }
+
         public string Lote { get; set; }
+
         public string ImagemURL { get; set; }
 
 
